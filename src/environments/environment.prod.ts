7032334cadcf6c1f9219d3a7b1621c19cbd299e6
GilbertCruz <gcruzin4@gmail.com>
2018-12-12 11:51:29 -0800

    anything

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBBwZkSs5zgWj1I5P1HpVDegS2b-knoCq4",
+    authDomain: "is445-course-project-95fab.firebaseapp.com",
+    databaseURL: "https://is445-course-project-95fab.firebaseio.com",
+    projectId: "is445-course-project-95fab",
+    storageBucket: "is445-course-project-95fab.appspot.com",
+    messagingSenderId: "375671009551",
+  },
+};
